a3f10c629b2b3930d11375fc1816cc78c9fc2c51
lcsmota <sacul_m@protonmail.com>
2023-02-10 16:20:57 -0300

    Feat: add ErrorValidation class

---
+++ CleanArchApi.Application/Validations/ErrorValidation.cs
@@ -0,0 +1,7 @@
+namespace CleanArchApi.Application.Validations;
+
+public class ErrorValidation
+{
+    public string Field { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+}

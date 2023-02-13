342aac0f1b66a138051c12fe20a2d693c1f9864e
lcsmota <sacul_m@protonmail.com>
2023-02-13 15:36:43 -0300

    Feat: Add property Category

--- CleanArchApi.Application/DTOs/ProductDTO.cs
+++ CleanArchApi.Application/DTOs/ProductDTO.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using CleanArchApi.Domain.Entities;
 
 namespace CleanArchApi.Application.DTOs;
 
@@ -37,4 +38,5 @@ public class ProductDTO
 
     [DisplayName("Categories")]
     public int CategoryId { get; set; }
+    public Category? Category { get; set; }
 }
